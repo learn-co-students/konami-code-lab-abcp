18216e32205f217095f111d0ba8af78cf90bce05
Azumi Callanan <azucm@hotmail.com>
2018-01-17 17:59:00 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+var i = 0;
 
+function init() {
+  document.body.addEventListener('keydown', function(e){
+    var key = parseInt(e.detail || e.which);
+    if(key === code[i]){
+      i++;
+      if(i === code.length - 1){
+        alert("Congrats!");
+        //reset the index counter
+        i = 0;
+      }
+    }else{
+      //if the key doesn't match current index, then reset it to zero
+      i = 0;
+    }
+  });
 }
