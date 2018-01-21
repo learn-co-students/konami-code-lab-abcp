765c50d9411396814cbcdb11d13671a659069e92
stanciuionelamelania@gmail.com <stanciuionelamelania@gmail.com>
2018-01-21 00:06:15 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,15 +1,10 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
-  document.body.addEventListener("keydown",onKeyDownHandler("keydown"),false)
-
-
-}
-  function onKeyDownHandler(e) {
-     let index = 0;
+  let index = 0;
+  document.body.addEventListener(`keydown`,function (e) {
     const key = parseInt(e.detail || e.which);
     if (key === code[index]) {
       index++;
-
       if (index === code.length) {
         alert("Hurray!");
 
@@ -20,3 +15,5 @@ function init() {
     }
 
   }
+)
+}
