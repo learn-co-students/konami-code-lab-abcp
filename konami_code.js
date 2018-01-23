ffe83ac9e8af2329a107d89273b041d7f246d18a
Sayaka Tamura <arccycle@gmail.com>
2018-01-23 01:27:25 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -7,5 +7,20 @@ function init()
   
   document.body.addEventListener('keydown',function(e){
     const key = parseInt(e.which || e.detail);
-  })
+    
+    if(code[index] === key)
+    {
+      index++;
+      
+      if(index === code.length)
+      {
+        alert('Hurray!');
+        index = 0;
+      }
+    }
+    else
+    {
+      index = 0;
+    }
+  });
 }
