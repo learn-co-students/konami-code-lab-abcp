205bc3ad8ad81512da8a97131d5104b9c847f2ea
leahbateman@gmail.com Bateman <leahbateman@gmail.com>
2018-01-28 05:51:37 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,29 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+let index = 0;
 
+function init() {
+	// attach a keydown event listener to the document body
+	document.body.addEventListener('keydown', testFunction);
+}
+
+function testFunction(e) {
+	// convert value of keypress into integer & save it in key variable
+	const key = parseInt(e.detail || e.which);
+ 
+	// check whether the key matches the current spot in the code array
+	if (key === code[index]) {
+		// if so, increment the index
+		index++;
+ 
+		// if the index is at the end of the array, send alert
+		if (index === code.length) {
+			alert("Congratulations!");
+ 
+			index = 0;
+		}
+	// if the key doesn't match the current spot in the array, reset the index to 0 to start over
+	} else {
+		index = 0;
+  }
 }
